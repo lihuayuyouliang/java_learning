438b1403fd04066b967f45d882966502cfbeab1a
hnu <1824413744@qq.com>
2023-10-15 17:08:40 +0800

    ceshi

--- src/thread/callable.java
+++ src/thread/callable.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class callable {
+
+    // ceshi
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MyThread myThread=new MyThread();
         FutureTask futureTask=new FutureTask(myThread);
